47a6e18f6fab10089220c3fd04328920c617a7b7
mJaJksJ <maksim.m00@mail.ru>
2020-11-18 10:43:10 +0300

    add double type int task 1 lab 5

--- lab5/task1/main.cpp
+++ lab5/task1/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main()
 {
     cout << "a - b = ?" << endl;
+    cout << "Float::\n";
     float a = 123456789;
     float b = 123456788;
     cout << "123456789 - 123456788 = " << a-b << endl;
@@ -17,5 +18,20 @@ int main()
     "\n float 123456789 as int: " << (int)(float)123456789 <<
     "\n float 123456788 as int: " << (int)(float)123456788 <<
     "\n float 123456787 as int: " << (int)(float)123456787 << endl;
+
+    cout << endl << endl << "Double::\n";
+    double aa = 123456789;
+    double bb = 123456788;
+    cout << "123456789 - 123456788 = " << aa-bb << endl;
+
+    a = 123456788;
+    b = 123456787;
+    cout << "123456788 - 123456787 = " << aa-bb << endl;
+
+    cout << "\n\nRemark" <<
+    "\n double 123456789 as int: " << (int)(double)123456789 <<
+    "\n double 123456788 as int: " << (int)(double)123456788 <<
+    "\n double 123456787 as int: " << (int)(double)123456787 << endl;
+
     return 0;
 }
