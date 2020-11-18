609c0ef49292295ef234cf51ccfed9fa24abacae
mJaJksJ <maksim.m00@mail.ru>
2020-11-18 13:16:16 +0300

    task 2 lab 5 done?

--- lab5/task2/main.cpp
+++ lab5/task2/main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    volatile const double a = 0.9;
+    double x;
+    volatile double y;
+    cout << "cos(x+0.9*x*x)=?" << endl;
+    cout << "Write x: ";
+    cin >> x;
+    asm
+    (                   //st(0)     st(1)
+    "fldl %[X]\n"       //X
+    "fmull %[X]\n"      //X*X
+    "fmull %[A]\n"      //0.9*X*X
+    "fldl %[X]\n"       //2*X*X,    X
+    "faddp \n"           //2*X*X+X
+    "fcos\n"            //cos(2*X*X+X)
+    "fstpl %[Y]\n"      //Y=cos(2*X*X+X), стек FPU пуст
+    :[Y]"=m"(y)
+    :[X]"m"(x), [A]"m"(a)
+    :"cc"
+    );
+    cout << "cos(" << x << "+" << "0.9*" << x << "*" << x << ")=" << y;
 }
