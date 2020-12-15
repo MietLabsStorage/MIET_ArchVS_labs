91b6b5832530e4a6c1b5e4f32be0e1a5eb41c1ac
Konstantin2702 <61342474+Konstantin2702@users.noreply.github.com>
2020-12-15 22:00:20 +0300

    Comments

--- lab7/task2/main.cpp
+++ lab7/task2/main.cpp
@@ -21,22 +21,22 @@ int main()
     int minElem = a[0];
     asm
     (
-    "movl $1, %%eax \n"
-    "movl %[a], %%edx \n"
-    "movl (%%edx), %%edx \n"
+    "movl $1, %%eax \n"  //counter
+    "movl %[a], %%edx \n"  //записываем  адрес? нулевого элемента в edx берем за основу как минимум начальный
+    "movl (%%edx), %%edx \n" //записываем значение 
 
     "Begin: \n"
-    "cmpl %[N], %%eax \n"
+    "cmpl %[N], %%eax \n"   //while < N
     "je End \n"
 
     "movl %[a], %%ecx \n"
-    "leal (%%ecx, %%eax, 4), %%ecx \n"
-    "movl (%%ecx),%%ecx \n"
+    "leal (%%ecx, %%eax, 4), %%ecx \n"   // записать адрес следующего элемента массива
+    "movl (%%ecx),%%ecx \n" //записать значение
 
-    "cmpl %%edx, %%ecx \n"
+    "cmpl %%edx, %%ecx \n"   //проверка на минимум, если не выполняется, то прыгаем на лейбл и прибавляем счетчик
     "jnbe lable \n"
 
-    "movl %%ecx, %%edx \n"
+    "movl %%ecx, %%edx \n" ///если меньше минимума, то записываем новый минимум в edx
 
     "lable: \n"
     "incl %%eax \n"
